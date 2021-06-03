db571e55174f492f6596ec1608631042de418d84
wxh0814 <wxh0814@outlook.com>
2021-06-04 00:18:35 +0800

    my first pratice

---
+++ Leetcode1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        int i,j;
+        for(i=0;i<nums.size()-1;i++)
+        {
+            for(j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    return {i,j};
+                }
+            }
+        }
+        return {i,j};
+    }
+};
+int main()
+{
+    Solution solution;
+    int target = 5;
+    vector<int> nums = {1, 2, 3};
+    vector<int> ans = solution.twoSum(nums, target);
+    for (auto iter = ans.begin(); iter != ans.end(); ++iter) {
+        cout << *iter << endl;
+    }
+}
